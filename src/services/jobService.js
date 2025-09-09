5ac36f086d44eff1ce46ba49797b20c1663ac215
Vikas <sicsdev21@gmail.com>
2025-09-10 01:50:17 +0530

    updates

--- src/services/jobService.js
+++ src/services/jobService.js
@@ -1,4 +1,4 @@
-const { bookGenerationQueue } = require('../config/queue');
+const { bookGenerationQueue, bookReGenerationQueue } = require('../config/queue');
 const logger = require('../utils/logger');
 
 /**
@@ -19,6 +19,20 @@ const queueJob = async (jobName, data) => {
     });
 };
 
+const queueReJob = async (jobName, data) => {
+    logger.info(`Adding job '${jobName}' to queue with data:`, data);
+    await bookReGenerationQueue.add(jobName, data, {
+        attempts: 1,
+        backoff: {
+            type: 'exponential',
+            delay: 1000,
+        },
+        removeOnComplete: 1000,
+        removeOnFail: 500
+    });
+};
+
 module.exports = {
-    queueJob
+    queueJob,
+    queueReJob
 };
